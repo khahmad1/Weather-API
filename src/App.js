46c0f30378a95df98e8082b5a5f3285d0b680865
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-19 11:28:43 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+/*import Search from "./components/Search";
 
 import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+import fakeWeatherData from "./fakeWeatherData.json";*/
 
 import "./App.css";
 
@@ -21,9 +21,65 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-
-       <h1>Hello we are Mahmoud khodor and Ahmad Kharma </h1>
+      <header>
+        <form action="">
+          <input className="search" type="text" name="search2" placeholder="Type in a city name"/>
+            <button type="submit">Find Weather</button>
+        </form>
+      </header>
+      <main className="app__main">
+      <figure>
+          <img src="http://placekitten.com/200/200" alt="cat"></img>
+          <figcaption>overcast clouds</figcaption>
+        </figure>
+        <div className="describeTemp">
+          <span>Temperature</span> 10&deg;to 11&deg;C
+        <div>
+          <p>
+            <span>Humidity</span> 78% <span>Pressure</span> 1008.48
+          </p>
+        </div>
+        </div>
+      </main>
+      <div className="dayDetails">
+        <article>
+          <time>03:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>06:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>09:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>12:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>15:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>18:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
+        <article>
+          <time>21:00</time>
+          <img src="http://placekitten.com/200/200" alt="cat" />
+          <span>8&deg;C</span>  
+        </article>
       </div>
+    </div>
+    
     );
   }
 }
